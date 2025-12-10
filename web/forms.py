dcf12d608dd9e61cd7060b3974d222601c0f2a24
nivethaaarjun-byte <nivetha.a.arjun@gmail.com>
2025-12-10 10:42:52 +0530

    Replace all Chinese form field labels with English

--- web/forms.py
+++ web/forms.py
@@ -16,65 +16,65 @@ class PageInfo():
 
 class SchoolForm(FlaskForm):
     id = HiddenField('id')
-    name = StringField('学校名称', validators=[Length(min=1, max=50)])  # 学校名称
-    area_id = SelectField(u'所在区县', coerce=int)  # 区县
-    teachdesc = TextAreaField(u'校长及教师情况')  #
-    address = StringField('地址')  #
-    schooltype_id = SelectField(u'学校类型', coerce=int)  #
-    website = StringField('网址')  #
-    distinguish = TextAreaField(u'教学特色')  #
-    leisure = TextAreaField(u'课外特色活动')  #
-    threashold = TextAreaField(u'招生条件及招生地块')  #
-    partner = StringField('对口学校')  #
-    artsource = StringField('艺术特长招生情况')  #
-    feedesc = StringField('学费标准')  #
-    longitude = DecimalField('经度', places=4)
-    latitude = DecimalField('纬度', places=4)
-    feature_ids = SelectMultipleField(u'教学特色', coerce=int)
+    name = StringField('School Name', validators=[Length(min=1, max=50)])
+    area_id = SelectField(u'District', coerce=int)
+    teachdesc = TextAreaField(u'Principal & Teachers Info')
+    address = StringField('Address')
+    schooltype_id = SelectField(u'School Type', coerce=int)
+    website = StringField('Website')
+    distinguish = TextAreaField(u'Teaching Features')
+    leisure = TextAreaField(u'Extracurricular Activities')
+    threashold = TextAreaField(u'Admission Requirements & Zones')
+    partner = StringField('Partner School')
+    artsource = StringField('Arts Enrollment')
+    feedesc = StringField('Tuition Fee')
+    longitude = DecimalField('Longitude', places=4)
+    latitude = DecimalField('Latitude', places=4)
+    feature_ids = SelectMultipleField(u'Teaching Features', coerce=int)
     image = FileField(
-        '上传图片', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+        'Upload Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
 
 
 class InstitutionForm(FlaskForm):
     id = HiddenField('id')
-    name = StringField('品牌名', validators=[Length(min=1, max=50)])
-    agespan_id = SelectField(u'招生年龄', coerce=int)
-    area_id = SelectField(u'所在区县', coerce=int)
-    address = StringField('地址')  #
-    location = StringField('校区名')
-    website = StringField('网址')  #
-    telephone = StringField('电话')
-    feedesc = StringField('学费标准')  #
-    timeopen = DateTimeField('开业时间', format='%H:%M')
-    timeclose = DateTimeField('关门时间', format='%H:%M')
-    feetype_id = SelectField('收费类型', coerce=int)
-    longitude = DecimalField('经度', places=4)
-    latitude = DecimalField('纬度', places=4)
-    # featuredesc = db.Column(db.String(200)) #特色小项描述
-    feature_ids = SelectMultipleField(u'培训方向', coerce=int)
+    name = StringField('Brand Name', validators=[Length(min=1, max=50)])
+    agespan_id = SelectField(u'Age Group', coerce=int)
+    area_id = SelectField(u'District', coerce=int)
+    address = StringField('Address')
+    location = StringField('Campus Name')
+    website = StringField('Website')
+    telephone = StringField('Phone')
+    feedesc = StringField('Tuition Fee')
+    timeopen = DateTimeField('Opening Time', format='%H:%M')
+    timeclose = DateTimeField('Closing Time', format='%H:%M')
+    feetype_id = SelectField('Fee Type', coerce=int)
+    longitude = DecimalField('Longitude', places=4)
+    latitude = DecimalField('Latitude', places=4)
+    # featuredesc = db.Column(db.String(200)) #Training specialties description
+    feature_ids = SelectMultipleField(u'Training Focus', coerce=int)
     image = FileField(
-        '上传图片', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+        'Upload Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
 
 
 class BulletinForm(FlaskForm):
     id = HiddenField('id')
-    dt = DateTimeField('发布时间', format='%Y-%m-%d %H:%M:%S')
-    title = StringField('标题')
-    content = TextAreaField('详情')
-    valid = BooleanField('是否有效')
-    source = StringField('来源')
-    author = StringField('作者')
+    dt = DateTimeField('Published Date', format='%Y-%m-%d %H:%M:%S')
+    title = StringField('Title')
+    content = TextAreaField('Content')
+    valid = BooleanField('Is Valid')
+    source = StringField('Source')
+    author = StringField('Author')
     image = FileField(
-        '上传图片', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+        'Upload Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
 
 
 class AccountForm(FlaskForm):
     id = HiddenField('id')
-    dtcreate = DateTimeField('注册时间', format='%Y-%m-%d %H:%M:%S')
-    username = StringField('登录名')
-    password = StringField('密码')
-    name = StringField('昵称')
-    telephone = StringField('注册电话')
-    flag_telephone = BooleanField('是否已认证')
-    checkcode = StringField('认证码')
-    source = StringField('来源')
+    dtcreate = DateTimeField('Registration Date', format='%Y-%m-%d %H:%M:%S')
+    username = StringField('Username')
+    password = StringField('Password')
+    name = StringField('Display Name')
+    telephone = StringField('Phone Number')
+    flag_telephone = BooleanField('Phone Verified')
+    checkcode = StringField('Verification Code')
+    source = StringField('Source')
